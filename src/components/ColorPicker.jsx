11f7aad7aef22fc3f7906ea11767855fa37a541c
Akash G <127029325+tea-cuppp@users.noreply.github.com>
2023-06-25 10:21:13 +0530

    Add files via upload

---
+++ src/components/ColorPicker.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import {SketchPicker} from "react-color"
+import state from '../store'
+import { useSnapshot } from 'valtio'
+
+
+const ColorPicker = () => {
+  const snap=useSnapshot(state);
+  return (
+    <div className='absolute left-full ml-3'>
+            <SketchPicker  
+            color={snap.color}
+              onChange={(color) =>state.color=color.hex}
+            />
+    </div>
+  )
+}
+
+export default ColorPicker
